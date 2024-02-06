ea5016636147d1f9e11d3bdf89e145cae472e8d7
carlosmilenaquesada <135043776+carlosmilenaquesada@users.noreply.github.com>
2024-02-06 05:16:17 +0100

    ultimo

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 9;
+    private float spawnPosZ = 20;
+
+    private float startDelay = 2;
+    private float spawnInterval = 2;
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+
+    void SpawnRandomAnimal()
+    {
+        //Generación aleatoria de animales
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
